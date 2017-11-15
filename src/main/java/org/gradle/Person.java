7d27745f16c5e0359874c7c35d5eb6f07ad2648c
wif_adm_kouhei.minami <k-minami@froide.co.jp>
2017-11-15 10:36:28 +0900

    項目名変更

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -3,14 +3,14 @@ package org.gradle;
 import org.apache.commons.collections.list.GrowthList;
 
 public class Person {
-    private final String name;
+    private final String sampleName;
 
     public Person(String name) {
-        this.name = name;
+        this.sampleName = name;
         new GrowthList();
     }
 
     public String getName() {
-        return name;
+        return sampleName;
     }
 }
